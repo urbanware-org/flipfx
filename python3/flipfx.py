4a341ad54b2a3724ede9dd1fa24dbc733eebd67c
urbanware-org <github@urbanware.org>
2019-03-25 21:16:29 +0100

    Raise version number

--- python3/flipfx.py
+++ python3/flipfx.py
@@ -10,7 +10,7 @@
 # GitLab: https://gitlab.com/urbanware-org/flipfx
 # ============================================================================
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 import random
 import time
